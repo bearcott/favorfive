bbeaede4688d0d5dfe0ca8f1affb047f6fa7dfb6
Bear Cott <bear@bearcott.com>
2014-10-19 09:19:00 -0500

    final changes bitches

--- public/js/controllers/FeedCtrl.js
+++ public/js/controllers/FeedCtrl.js
@@ -1,4 +1,4 @@
-angular.module('FeedCtrl', []).controller('FeedController', function($scope) {
+angular.module('FeedCtrl', ['ngDraggable']).controller('FeedController', function($scope) {
 	$scope.user = Parse.User.current();
 	$scope.emailHash = md5($scope.user.attributes.email);
 
@@ -7,31 +7,21 @@ angular.module('FeedCtrl', []).controller('FeedController', function($scope) {
 		user = Parse.User.current();
 		thing = new Thing();
 		thing.set('favor',favor.name);
+		thing.set('username',user.attributes.username);
 		thing.set('description',favor.desc);
 		thing.set('time',new Date().getTime());
 		thing.set('email',user.attributes.email);
 		thing.set('emailHash',md5(user.attributes.email));
 		thing.set('phone',user.attributes.phone);
-		thing.save();
+		thing.save({
+			success: makeStuff()
+		});
 	}
-	$scope.convertToDate = function (stringDate){
-	  var dateOut = new Date(stringDate);
-	  dateOut.setDate(dateOut.getDate() + 1);
-	  return dateOut;
-	};
-	var stuff = Parse.Object.extend("Favor");
-	var stuffs = new Parse.Query(stuff);
-	stuffs.find({
-		success: function(results) {
-			$scope.favors = results;
-			$scope.$apply();
-		},
-		error: function(error) {
-			alert("Error: " + error.code + " " + error.message);
-		}
-	});
-	setInterval(function() {
-		stuffs.find({
+
+	function makeStuff() {
+		var stuff = Parse.Object.extend("Favor");
+		var stuffs = new Parse.Query(stuff);
+		return stuffs.find({
 			success: function(results) {
 				$scope.favors = results;
 				$scope.$apply();
@@ -40,6 +30,34 @@ angular.module('FeedCtrl', []).controller('FeedController', function($scope) {
 				alert("Error: " + error.code + " " + error.message);
 			}
 		});
-	},500);
+	}
+	makeStuff()
+
+	$scope.centerAnchor = true;
+	$scope.toggleCenterAnchor = function () {$scope.centerAnchor = !$scope.centerAnchor}
+	$scope.draggableObjects = [];
+	$scope.droppedObjects = [];
+	var stuff = Parse.Object.extend("Favor");
+	var stuffs = new Parse.Query(stuff);
+	stuffs.find({
+		success: function(results) {
+			for(var i=0; i<=results.length; i++) {
+				$scope.draggableObjects.push(results[i]);
+			}
+			$scope.onDropComplete=function(data,evt){
+		        var index = $scope.droppedObjects.indexOf(data);
+		        if (index == -1)
+		        $scope.droppedObjects.push(data);
+		        console.log($scope.droppedObjects);
+		    }
+		    $scope.onDragSuccess=function(data,evt){
+		        var index = $scope.droppedObjects.indexOf(data);
+		        if (index > -1) {
+		            $scope.droppedObjects.splice(index, 1);
+		        }
+		    }
+		}
+	});
+
 
 });
