c2c3ed2956240f8c87e7451adec67adaa324a309
Bear Cott <bear@bearcott.com>
2014-10-18 19:35:17 -0500

    lol init

---
+++ app/routes.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+
+	// server routes ===========================================================
+	// handle things like api calls
+	// authentication routes
+
+	// frontend routes =========================================================
+	// route to handle all angular requests
+	app.get('/', function(req, res) {
+		res.sendfile('./public/index.html');
+	});
+	app.get('/login', function(req, res) {
+		res.sendfile('./public/index.html');
+	});
+};
