4ba470a5411714fb945d7a14fa34391a03810ba7
TAHIR_AZIZ <tahir.aziz4@gmail.com>
2014-10-18 23:19:19 -0500

    signUpPage

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,6 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-	$scope.tagline = 'Learn. Inspire. Be Inspired.';	
-
+	$scope.tagline = 'Learn. Inspire. Be Inspired.';
+	$scope.description = 'Find a peer on campus that can help you.';
+	$scope.learn = 'Learn More';
 });
